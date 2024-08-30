66b1ca0425d1870042713913f2077b51dec7117e
Akash Regmi <akashregmi82@gmail.com>
2024-08-30 16:53:45 +0545

    Adding the React Button

--- React_ToDo_App/src/App.jsx
+++ React_ToDo_App/src/App.jsx
@@ -1,43 +1,13 @@
 import { useState } from "react";
 
 import "./App.css";
+import Todo from "./Todo";
 
 function App() {
-  const [todos, setTodos] = useState(["here", "there"]);
 
-  const handelsubmit = (e) => {
-    e.preventDefault();
-    setTodos([...todos, e.target[0].value]);
-    e.target.reset();
-  };
-  const handelDelete = (index) => {
-    const newTodos = todos.filter((todo, i) => i !== index);
-    setTodos(newTodos);
-  };
   return (
     <>
-      <form onSubmit={handelsubmit}>
-        <input type="text" required />
-        <input type="submit" value="Add To Do" />
-      </form>
-      <ul>
-        {todos.map((todo, index) => {
-          return (
-            <li key={index}>
-              {" "}
-              {todo}
-              <button
-                onClick={() => {
-                  handelDelete(index);
-                }}
-              >
-                Delete
-              </button>
-              <button>Edit</button>
-            </li>
-          );
-        })}
-      </ul>
+      < Todo/>
     </>
   );
 }
